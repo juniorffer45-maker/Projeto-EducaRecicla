7f23f3c08243cf21960f845ff0a366c900696d56
Jopa-en <joaaoppaulo17@gmail.com>
2025-12-11 16:23:39 -0300

    Atualizar o Classe Doacão:João Paulo.py

--- ClasseConfiguraçoesJoão Paulo.py
+++ ClasseConfiguraçoesJoão Paulo.py
@@ -1,1 +1,11 @@
 
+    Representa a transação de doação.
+    Ela associa um Doador, um Receptor e os Materiais envolvidos.
+    """ Classe doação:
+    def __init__(self, codigo_doacao: str, doador, receptor, materiais):
+
+        # Atributos específicos da transação
+        self.codigo_doacao = codigo_doacao 
+        self.nota = None           # Nota [int]
+        self.desc_av = None        # Descrição/Avaliação [string(500)]
+        self.categoria_doacao = None # Categoria da Doação [string]
