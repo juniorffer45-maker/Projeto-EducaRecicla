95e1868c58ee61914a792bdf3f5139fef9525832
JuniorFerr <juniorffer45@gmail.com>
2025-12-11 22:05:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -93,5 +93,13 @@ class Material: #Criada por Aldemir Ferreira da Silva Junior
 
 
 
+class Doação:
+    def __init__(self, codigo_doacao: str, doador, receptor, materiais):
+
+        # Atributos específicos da transação
+        self.codigo_doacao = codigo_doacao 
+        self.nota = None           # Nota [int]
+        self.desc_av = None        # Descrição/Avaliação [string(500)]
+        self.categoria_doacao = None # Categoria da Doação [string]
 
 
