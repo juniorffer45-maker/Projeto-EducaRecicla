ecce5a1d82e35aa3928665cad8be47818e4e7166
Beatriz Benigno de Vasconcelos <benigno.beatriz@aluno.ufca.edu.br>
2026-01-30 22:54:38 -0300

    Delete classeadm.py

--- classeadm.py
+++
@@ -1,9 +0,0 @@
-
-    # NOVA PROPRIEDADE: Começa sempre como Falso (Usuário comum)
-        self.admin = False 
-
-    def definir_como_administrador(self):
-        #Transforma este usuário em um administrador.
-        self.admin = True
-        print(f"Permissão de ADMIN concedida ao usuário: {self.nome}")
-
