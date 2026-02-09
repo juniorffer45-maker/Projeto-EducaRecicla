9e60fc39cf8605af4825ec9d10dcbe35f72a3969
JuniorFerr <juniorffer45@gmail.com>
2026-02-08 21:17:51 -0300

    Update bancodedados.sql

--- bancodedados.sql
+++ bancodedados.sql
@@ -1,165 +1,169 @@
+-- 1. Tabelas Base
 CREATE TABLE Localidade (
-    id_localidade INTEGER PRIMARY KEY,
-    nome_cidade VARCHAR(100) UNIQUE,
-    sigla_cidade VARCHAR(100) UNIQUE
+    id_localidade INTEGER GENERATED ALWAYS AS IDENTITY,
+    nome_cidade VARCHAR(100) UNIQUE NOT NULL,
+    sigla_cidade VARCHAR(100) UNIQUE NOT NULL,
+    CONSTRAINT pk_localidade PRIMARY KEY (id_localidade)
 );
 
 CREATE TABLE categoria (
-    id_categoria int generated always as identity,
-    nome varchar(100),
-    constraint pk_categoria primary key (id_categoria)
+    id_categoria INTEGER GENERATED ALWAYS AS IDENTITY,
+    nome VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_categoria PRIMARY KEY (id_categoria)
 );
 
-
+-- 2. Tabela de Usuário
 CREATE TABLE usuario (
-    id_usuario int generated always as identity, 
-    id_localidade int,
-    nome_completo varchar(100) not null,
-    email varchar (100) not null unique,
-    senha varchar(255) not null,
-    biografia text,
-    foto_perfil_url varchar(500),
-    data_cadastro timestamp default current_timestamp,
-    telefone varchar(20) unique,
-    is_whatsapp boolean default false,
-    reputacao_media decimal(4,2) default 0.0,
+    id_usuario INTEGER GENERATED ALWAYS AS IDENTITY,
+    id_localidade INTEGER,
+    nome_completo VARCHAR(100) NOT NULL,
+    email VARCHAR (100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    biografia TEXT,
+    foto_perfil_url VARCHAR(500),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    telefone VARCHAR(20) UNIQUE,
+    is_whatsapp BOOLEAN DEFAULT FALSE,
+    reputacao_media DECIMAL(4,2) DEFAULT 0.0,
     
-    constraint pk_usuario primary key (id_usuario),
-    constraint fk_localidade foreign key (id_localidade)
-        references localidade (id_localidade)
-        on update cascade
-        on delete set null
+    CONSTRAINT pk_usuario PRIMARY KEY (id_usuario),
+    CONSTRAINT fk_localidade FOREIGN KEY (id_localidade)
+        REFERENCES localidade (id_localidade)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL
 );
 
-
+-- 3. Tabelas de Itens e Anúncios
 CREATE TABLE material (
-    id_material int generated always as identity,
-    categoria_id int not null,
-    dono_id int not null,
-    nome_item varchar(100),
-    descricao text,
-    estado_conservacao varchar(50),
-    quantidade int default 1 check (quantidade > 0), 
+    id_material INTEGER GENERATED ALWAYS AS IDENTITY,
+    categoria_id INTEGER NOT NULL,
+    dono_id INTEGER NOT NULL,
+    nome_item VARCHAR(100),
+    descricao TEXT,
+    estado_conservacao VARCHAR(50),
+    quantidade INTEGER DEFAULT 1 CHECK (quantidade > 0), 
     
-    constraint pk_material primary key (id_material),
-    constraint fk_categoria foreign key (categoria_id)
-        references categoria (id_categoria)
-        on update cascade
-        on delete restrict,
-    constraint fk_us_dono foreign key (dono_id)
-        references usuario (id_usuario)
-        on update cascade
-        on delete cascade,
-    constraint ck_estado check (estado_conservacao in ('Novo', 'Bom', 'Usado'))
+    CONSTRAINT pk_material PRIMARY KEY (id_material),
+    CONSTRAINT fk_categoria FOREIGN KEY (categoria_id)
+        REFERENCES categoria (id_categoria)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_us_dono FOREIGN KEY (dono_id)
+        REFERENCES usuario (id_usuario)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT ck_estado CHECK (estado_conservacao IN ('Novo', 'Bom', 'Usado'))
 );
 
 CREATE TABLE anuncio (
-    id_anuncio int generated always as identity,
-    id_usuario int not null,
-    id_material int not null,
-    titulo varchar(100) not null,
-    data_public timestamp default current_timestamp,
-    data_expi timestamp,
-    status varchar (20) default 'ATIVO',
+    id_anuncio INTEGER GENERATED ALWAYS AS IDENTITY,
+    id_usuario INTEGER NOT NULL,
+    id_material INTEGER NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
+    data_public TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_expi TIMESTAMP,
+    status VARCHAR (20) DEFAULT 'ATIVO',
     
-    constraint pk_anuncio primary key (id_anuncio),
-    constraint fk_usuario foreign key (id_usuario)
-        references usuario (id_usuario)
-        on update cascade
-        on delete cascade,
-    constraint fk_material foreign key (id_material)
-        references material (id_material)
-        on update cascade
-        on delete cascade,
-    constraint ck_status check (status in ('ATIVO', 'PAUSADO', 'FINALIZADO'))
+    CONSTRAINT pk_anuncio PRIMARY KEY (id_anuncio),
+    CONSTRAINT fk_usuario FOREIGN KEY (id_usuario)
+        REFERENCES usuario (id_usuario)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT fk_material FOREIGN KEY (id_material)
+        REFERENCES material (id_material)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT ck_status CHECK (status IN ('ATIVO', 'PAUSADO', 'FINALIZADO'))
 );
 
-
+-- 4. Tabelas de Apoio e Interação
 CREATE TABLE foto_material (
-    id_foto int generated always as identity,
-    id_material int not null,
-    url_arquivo varchar(500) not null,
-    data_upload timestamp default current_timestamp,
-    constraint pk_foto primary key (id_foto),
-    constraint fk_material_foto foreign key (id_material)
-        references material (id_material)
-        on update cascade
-        on delete cascade
+    id_foto INTEGER GENERATED ALWAYS AS IDENTITY,
+    id_material INTEGER NOT NULL,
+    url_arquivo VARCHAR(500) NOT NULL,
+    data_upload TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_foto PRIMARY KEY (id_foto),
+    CONSTRAINT fk_material_foto FOREIGN KEY (id_material)
+        REFERENCES material (id_material)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 CREATE TABLE PontoColeta (
-    id_ponto_coleta INTEGER PRIMARY KEY,
-    id_localidade INTEGER Not Null,
+    id_ponto_coleta INTEGER GENERATED ALWAYS AS IDENTITY,
+    id_localidade INTEGER NOT NULL,
     nome_local VARCHAR(100),
     endereco_completo VARCHAR(100),
     horario_funcionamento VARCHAR(100),
+    CONSTRAINT pk_ponto_coleta PRIMARY KEY (id_ponto_coleta),
     CONSTRAINT fk_id_localidade_ponto
         FOREIGN KEY (id_localidade)
-        REFERENCES localidade(id_localidade)    
+        REFERENCES localidade(id_localidade)   
         ON UPDATE CASCADE
         ON DELETE RESTRICT
 );
 
 CREATE TABLE doacao (
-    id_doacao int generated always as identity,
-    doador_id int not null,
-    receptor_id int not null,
-    pontocoleta_id int not null,
-    anuncio_id int unique,
-    forma_entrega varchar(50),
-    status varchar(50) default 'PENDENTE',
-    data_confirmacao timestamp,
+    id_doacao INTEGER GENERATED ALWAYS AS IDENTITY,
+    doador_id INTEGER NOT NULL,
+    receptor_id INTEGER NOT NULL,
+    pontocoleta_id INTEGER NOT NULL,
+    anuncio_id INTEGER UNIQUE,
+    forma_entrega VARCHAR(50),
+    status VARCHAR(50) DEFAULT 'PENDENTE',
+    data_confirmacao TIMESTAMP,
     
-    constraint pk_doacao primary key (id_doacao),
-    constraint fk_doador foreign key (doador_id) references usuario (id_usuario),
-    constraint fk_receptor foreign key (receptor_id) references usuario (id_usuario),
-    constraint fk_ponto foreign key (pontocoleta_id) references PontoColeta (id_ponto_coleta),
-    constraint fk_anuncio foreign key (anuncio_id) references anuncio (id_anuncio),
-    constraint ck_doacao_status CHECK (status IN ('PENDENTE', 'AGENDADO', 'CONCLUIDO', 'CANCELADO')),
-    constraint ck_doador_diferente_receptor CHECK (doador_id <> receptor_id)
+    CONSTRAINT pk_doacao PRIMARY KEY (id_doacao),
+    CONSTRAINT fk_doador FOREIGN KEY (doador_id) REFERENCES usuario (id_usuario),
+    CONSTRAINT fk_receptor FOREIGN KEY (receptor_id) REFERENCES usuario (id_usuario),
+    CONSTRAINT fk_ponto FOREIGN KEY (pontocoleta_id) REFERENCES PontoColeta (id_ponto_coleta),
+    CONSTRAINT fk_anuncio FOREIGN KEY (anuncio_id) REFERENCES anuncio (id_anuncio),
+    CONSTRAINT ck_doacao_status CHECK (status IN ('PENDENTE', 'AGENDADO', 'CONCLUIDO', 'CANCELADO')),
+    CONSTRAINT ck_doador_diferente_receptor CHECK (doador_id <> receptor_id)
 );
 
 CREATE TABLE Mensagem (
-    id_mensagem Integer PRIMARY KEY,
+    id_mensagem INTEGER GENERATED ALWAYS AS IDENTITY,
     id_remetente INTEGER,
     id_destinatario INTEGER,
     id_anuncio INTEGER,
     conteudo TEXT,
-    data_envio timestamp default current_timestamp,
+    data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     lida BOOLEAN DEFAULT FALSE,
 
+    CONSTRAINT pk_mensagem PRIMARY KEY (id_mensagem),
     CONSTRAINT fk_id_remetente FOREIGN KEY (id_remetente) REFERENCES usuario(id_usuario),
     CONSTRAINT fk_id_destinatario FOREIGN KEY (id_destinatario) REFERENCES usuario(id_usuario),
     CONSTRAINT fk_id_anuncio_msg FOREIGN KEY (id_anuncio) REFERENCES anuncio(id_anuncio)
 );
 
 CREATE TABLE Notificacao (
-    id_notificacao INTEGER PRIMARY KEY,
+    id_notificacao INTEGER GENERATED ALWAYS AS IDENTITY,
     id_usuario INTEGER NOT NULL,
     id_origem INTEGER NOT NULL,
-    data_criacao timestamp default current_timestamp,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     titulo VARCHAR(50) NOT NULL,
     mensagem VARCHAR(255) NOT NULL,
     lida BOOLEAN DEFAULT FALSE,
     tipo VARCHAR(50),
     
+    CONSTRAINT pk_notificacao PRIMARY KEY (id_notificacao),
     CONSTRAINT fk_notif_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
     CONSTRAINT fk_notif_anuncio FOREIGN KEY (id_origem) REFERENCES anuncio(id_anuncio)
 );
 
 CREATE TABLE Moderacao (
-    id_moderacao INTEGER NOT NULL PRIMARY KEY,
+    id_moderacao INTEGER GENERATED ALWAYS AS IDENTITY,
     id_anuncio INTEGER NOT NULL,
     id_usuario_reportante INTEGER NOT NULL,
     id_usuario_denunciado INTEGER NOT NULL,
     motivo VARCHAR(100) NOT NULL,
     status_resolucao VARCHAR(50) NOT NULL,
-    data_registro timestamp default current_timestamp,
+    data_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     descricao TEXT NOT NULL,
     
+    CONSTRAINT pk_moderacao PRIMARY KEY (id_moderacao),
     CONSTRAINT fk_mod_anuncio FOREIGN KEY (id_anuncio) REFERENCES anuncio(id_anuncio),
     CONSTRAINT fk_mod_reportante FOREIGN KEY (id_usuario_reportante) REFERENCES usuario(id_usuario),
     CONSTRAINT fk_mod_denunciado FOREIGN KEY (id_usuario_denunciado) REFERENCES usuario(id_usuario)
 );
-
-
